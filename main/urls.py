56dba6def4fa369abdae5c5540f16560c48859e0
Andrey <andrei_novichihin@mail.ru>
2025-05-20 17:34:04 +0300

    feeds btn

--- main/urls.py
+++ main/urls.py
@@ -332,6 +332,7 @@ urlpatterns = [
     path('feeds/', include([
         path('avito/', views.AvitoFeed.as_view(), name='avito-feed'),
         path('avito-new/', views.NewAvitoFeed.as_view(), name='avito-feed'),
+        path('avito-new/<int:complex_id>', views.NewAvitoFeedByComplex.as_view(), name='avito-feed-by-complex'),
         path('domclick/', views.DomClickFeed.as_view(), name='domclick-feed'),
         path('cian/', views.CianFeedNew.as_view(), name='cian-feed')
         
